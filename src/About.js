42a211dd40db2cd8eb07de019bc4da418fb41afb
Larry Skuse <larry.skuse@gmail.com>
2023-10-24 17:12:37 -0400

    Improve About page wording

--- src/About.js
+++ src/About.js
@@ -16,16 +16,18 @@ export default function About() {
         <h2>What is branch prediction?</h2>
         <p className="about-spacing">
           Branch prediction is the act of predicting if a branch will be taken 
-          or not taken before the branch outcome is known. When a CPU receives 
-          a branch instruction it does not know if it will be taken or not, so 
-          it takes a guess. If it finds out later that its guess was wrong, 
-          then it pays a misprediction penalty because it wasted cycles on the 
-          speculatively executed instructions and needs to start over using the
-          correct instructions. Generally speaking, the fewer mispredictions, 
-          the better the branch predictor. However, branch predictor success 
-          varies depending on the pattern of branch outcomes the predictor 
-          encounters. Therefore, a good branch predictor will result in fewer 
-          branch mispredictions on a variety of branch outcome patterns.
+          or not taken before the actual branch outcome is known. When a CPU 
+          fetches a branch instruction, it does not know which direction it 
+          will go, so it asks its branch predictor to take a guess. If it 
+          finds out later that the guess was wrong, then it pays a 
+          misprediction penalty because it wasted cycles on the speculatively 
+          executed instructions and needs to start over using the correct 
+          instructions.
+        </p>
+        <p>
+          The goal of a branch predictor is to minimize the number of 
+          mispredictions. Good branch predictors work well on a variety of 
+          branch outcome patterns.
         </p>
         <h2>What branch predictors are available for simulation?</h2>
         <p className="about-spacing">
@@ -43,14 +45,14 @@ export default function About() {
             not taken predictor. It predicts taken every time. 
           </li>
           <li className="about-spacing">
-            <strong>One-bit:</strong> This predictor, also known as a 1-bit
+            <strong>One-bit:</strong> This predictor, also known as a one-bit
             saturating counter, remembers the last branch outcome and uses it 
             to predict the next branch outcome. This information is stored in 
             a single bit. In this simulator, one-bit predictors always start 
             by assuming the last branch was not taken.
           </li>
           <li className="about-spacing">
-            <strong>Two-bit:</strong> This predictor, also known as a 2-bit 
+            <strong>Two-bit:</strong> This predictor, also known as a two-bit 
             saturating counter, has four states: (1) strongly not taken, (2) 
             weakly not taken, (3) weakly taken, and (4) strongly taken. When a 
             branch is not taken, it will move one state towards the strongly 
@@ -60,23 +62,20 @@ export default function About() {
             weakly taken state. Similarly, when a branch is taken, it will 
             move one state towards the strongly taken state unless it is 
             already in the strongly taken state. When making a prediction, if 
-            the 2-bit branch predictor is in a strongly not taken or weakly 
+            the two-bit branch predictor is in a strongly not taken or weakly 
             not taken state, it will predict not taken, and if it is in a 
             strongly taken or weakly taken state, it will predict taken. In 
-            this simulator, the 2-bit predictor always starts in the strongly 
-            not taken state.
+            this simulator, the two-bit predictor always starts in the 
+            strongly not taken state.
           </li>
           <li className="about-spacing">
-            <strong>Two-level:</strong> You can set up to 10-bits of history 
+            <strong>Two-level:</strong> You can set up to 10 bits of history 
             when using the one-bit and two-bit predictors. When you use N 
             history bits, the last N branch outcomes will be stored in an 
             integer. Not taken branches are represented with zeros and taken 
             branches are represented with ones. This integer is then used to 
             index into an array of one-bit or two-bit predictors that 
-            ultimately predict the outcome of the next branch. The history
-            integer used to index into the array is the first-level of the 
-            predictor, and the one or two-bit predictor at that index is the 
-            second-level.
+            ultimately predict the outcome of the next branch.
           </li>
         </ul>
         <h2>How does this simulator work?</h2>
